f8468063c1fc36676aa089a12ab17195c513cad2
Sebaslri <arielloor.22@hotmail.com>
2026-08-20 12:24:56 -0500

    actualizacion environment backend

--- FRONTEND/src/app/environments/environment.prod.ts
+++ FRONTEND/src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   environment: "prod",
-  api: "https://frutas-backend.onrender.com/",
+  api: "https://frutas-backend234.onrender.com/",
   mlApi: "https://frutas-ia.onrender.com/"
 };
--- FRONTEND/src/app/environments/environment.ts
+++ FRONTEND/src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   environment: "local",
-  api: "https://frutas-backend.onrender.com/",
+  api: "https://frutas-backend234.onrender.com/",
   mlApi: "https://frutas-ia.onrender.com/"
 };
